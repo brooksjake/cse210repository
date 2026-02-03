e2f1c8fb4ab2445bf8a92238f222da9e5f4e3c20
brooksjake <jacobsb2003@gmail.com>
2026-02-03 00:39:04 -0700

    learning 5

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Console.Clear();
+        
+        Square sq = new Square("blue", 4);
+        Console.WriteLine(sq.GetColor());
+        Console.WriteLine(sq.GetArea());
+        
+        Rectangle rec = new Rectangle("green", 3,5);
+        Console.WriteLine(rec.GetColor());
+        Console.WriteLine(rec.GetArea());
+        
+        Circle circ = new Circle("pink", 2);
+        Console.WriteLine(circ.GetColor());
+        Console.WriteLine(circ.GetArea());
     }
 }
